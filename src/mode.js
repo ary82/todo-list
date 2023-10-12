2c8d06600371ac9c9091a2381caf33ebe9c7bdbd
Aryan Goyal <137564277+ary82@users.noreply.github.com>
2023-10-12 23:55:00 +0530

    add mode js

---
+++ src/mode.js
@@ -0,0 +1,14 @@
+import displayHandler from "./display.js";
+const modeButtons = document.querySelectorAll("#nav_bar > h3");
+export let currentMode = "upcoming";
+modeButtons.forEach((element) => {
+  element.addEventListener("click", () => {
+    clearClass();
+    element.classList.toggle("active");
+    currentMode = element.id;
+    displayHandler();
+  });
+});
+function clearClass() {
+  modeButtons.forEach((e) => e.classList.remove("active"));
+}
